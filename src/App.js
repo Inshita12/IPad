a4110f7bdc9c79a60263527b28655aa03606580c
Inshita Bamba <inshitabamba@gmail.com>
2021-07-04 16:44:50 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import Wheel from "./Components/Wheel";
+import Screen from "./Components/Screen";
+import Coverflow from "./Components/Coverflow";
+import Music from "./Components/Music";
 
-function App() {
-  return (
-    <div className="App">
+import Games from "./Components/Games";
+import Settings from "./Components/Settings";
+import Album from './Components/Album';
+import Artist from "./Components/Artist";
+import Song from "./Components/Song";
+import Second from './Components/Second';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    // Initial state as the first screen is home screen
+    this.state = {
+      home: "Screen",
+    };
+  }
+  //Var to handle the screen onClick
+  handleClick = (inner) => {
+    this.setState({
+      home: inner,
+    });
+  };
 
-
-    </div>
-  );
+  render() {
+    const { home } = this.state; 
+    const { handleClick } = this;
+    return (
+      // When value of home var changes the component as per the value renders
+      <div className="App">
+        
+        {home==="Screen"  && <Screen home={handleClick}/>} 
+        
+        {home === "Coverflow" && <Coverflow home={handleClick} />}
+        {home === "Music" && <Music home={handleClick} />}
+        {home === "Games" && <Games home={handleClick} />}
+        {home === "Settings" && <Settings home={handleClick} />}
+        {home === "Album" && <Album home={handleClick} />}
+        {home === "Artist" && <Artist home={handleClick} />}
+        { home === "Song" && <Song home={handleClick}/>}
+        {home==="Second" && <Second home={handleClick}/>}
+        <Wheel home={handleClick} />
+  
+      </div>
+      
+    );
+  }
 }
 
 export default App;
